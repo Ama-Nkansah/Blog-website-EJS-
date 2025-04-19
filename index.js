79095a7fecda83be14b60dcad2bb41c29bcf11cc
Ama Nkansah <amankansahstate@gmail.com>
2025-04-19 02:37:24 +0100

    rendered my EJS file

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from 'express';
+import { dirname } from "path"
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
-const port = 3000
+const port = 7000
+
+
+app.set('view engine', 'ejs'); 
+app.set('views', __dirname); 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render("main");
 })
 
 app.listen(port, () => {
