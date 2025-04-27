6cd88c8c1352a73786d9d10da1624ee91bba149a
Ama Nkansah <amankansahstate@gmail.com>
2025-04-27 21:11:14 +0100

    created the post method section

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 import express from 'express';
 import { dirname } from "path"
 import { fileURLToPath } from "url";
+import bodyParser from 'body-parser';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 7000
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs'); 
 app.set('views', __dirname); 
+app.use(express.static('public'));
+
+
+const posts = []; 
 
 app.get('/', (req, res) => {
-  res.render("main");
-})
+  res.render('main', { posts });
+});
+
+app.post('/create-post', (req, res) => {
+  const { title, content } = req.body; 
+  const newPost = { title, content }; 
+  posts.push(newPost); 
+  res.redirect('/'); 
+});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
